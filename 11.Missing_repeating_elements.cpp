773d4a7335277dab011892b04a604dba2f36359f
Punith Kumar <66864406+PunithKumar2902@users.noreply.github.com>
2022-06-09 10:58:57 +0530

    find missing and repeating elements

---
+++ 11.Missing_repeating_elements.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	    int xor1;
+
+    /* Will have only single set bit of xor1 */
+    int set_bit_no;
+
+    int i;
+    int x = 0; // missing
+    int y = 0; // repeated
+    
+
+    xor1 = arr[0];
+
+    /* Get the xor of all array elements */
+    for (i = 1; i < n; i++)
+        xor1 = xor1 ^ arr[i];
+
+    /* XOR the previous result with numbers from 1 to n */
+    for (i = 1; i <= n; i++)
+        xor1 = xor1 ^ i;
+
+    set_bit_no = xor1 & ~(xor1 - 1);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] & set_bit_no)
+            x = x ^ arr[i];
+
+        else
+            y = y ^ arr[i];
+    }
+
+    for (i = 1; i <= n; i++) {
+        if (i & set_bit_no)
+            /* i belongs to first set */
+            x = x ^ i;
+
+        else
+            /* i belongs to second set */
+            y = y ^ i;
+    }
+
+    int x_count = 0;
+    for (int i=0; i<n; i++) {
+        if (arr[i]==x)
+            x_count++;
+    }
+    
+    if (x_count==0)
+        return {x, y};
+    
+    return {y, x};
+}
+
