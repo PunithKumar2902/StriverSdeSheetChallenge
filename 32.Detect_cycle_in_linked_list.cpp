9f263791d9ee0b8d7a1f1fc84f39c5c2b83c782d
Punith Kumar <66864406+PunithKumar2902@users.noreply.github.com>
2022-06-16 14:28:43 +0530

    detect a cycle in linked list

--- 32.Detect_cycle_in_linked_list.cpp
+++ 32.Detect_cycle_in_linked_list.cpp
@@ -1,24 +1,15 @@
 #include <bits/stdc++.h> 
 
-int findIntersection(Node *l1, Node *l2)
+bool detectCycle(Node *head)
 {
-    Node *temp1=l1,*temp2=l2;
-    while(temp1!=temp2)
+    Node* slow=head,*fast=head;
+    
+    while(fast!=NULL and fast->next!=NULL)
     {
-        if(temp1==NULL)
-        {
-            temp1=l2;
-        }
-        else 
-            temp1=temp1->next;
-        if(temp2==NULL)
-        {
-            temp2=l1;
-        }
-        else
-            temp2=temp2->next;
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+            return true;
     }
-    if(temp1)
-        return temp1->data;
-    return -1;
+    return false;
 }
